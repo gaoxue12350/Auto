f4573ab66cbfa0344c5393829499190b46145f9a
gaoxue <973140551@qq.com>
2021-01-08 20:12:46 +0800

    接口自动化

---
+++ AutoTest/web/podemo1/page/base_page.py
@@ -0,0 +1,40 @@
+import shelve
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class BasePage:
+    base_url = ''
+
+    def __init__(self, driver: WebDriver = None):
+        if driver == None:
+            # 第一次初始化
+            options = Options()
+            options.debugger_address = '127.0.0.1:9222'
+            self.driver = webdriver.Chrome(options=options)
+            # cookies=self.driver.get_cookies()
+            # db=shelve.open('cookie')
+            # db['cookie']=cookies
+            # db.close()
+            # self.driver = webdriver.Remote('http://127.0.0.1:5001/wd/hub')
+            self.driver.implicitly_wait(20)
+        else:
+            self.driver = driver
+        if self.base_url != '':
+            self.driver.get(self.base_url)
+
+    def find(self, by, locator):
+        return self.driver.find_element(by, locator)
+
+    def finds(self, by, locator):
+        return self.driver.find_elements(by, locator)
+
+    def wait_for_click(self, locator, timeout=10):
+        element: WebElement = WebDriverWait(self.driver, timeout).until(
+            expected_conditions.element_to_be_clickable(locator))
+        return element
